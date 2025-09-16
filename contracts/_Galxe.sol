db4cf19a13c4d5ebd50d44e3e3c72a5a8cad2e86
anxbt <anubrat23@gmail.com>
2025-09-16 18:54:07 +0530

    v2

---
+++ contracts/_Galxe.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Galxe{
+
+    address token;
+    uint256 budget;
+    uint256 endTime;
+
+    function createCampaign(token, budget, endTime)external payable{
+
+    }
+
+ function closeCampaign() external{
+
+ }
+
+}
